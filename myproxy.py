f49a817bfcd892cfafa75f7ad80bb277566c2ed0
sektor <sektorif@users.noreply.github.com>
2020-10-12 11:08:10 +0900

    first commit

--- myproxy.py
+++ myproxy.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello FIIT-17!'
+	if 'url' in request.args:
+		r = requests.get(request.args['url'])
+		bts = r.content
+		return bts
+	else :
+		return '''
+		
+			<form method = 'get'>
+			<input name = "url" type = "text">
+			<input type = "submit">
+			</form>
+			
+		'''
 
 if __name__ == '__main__':
     app.run()
